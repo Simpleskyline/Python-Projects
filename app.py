6db3ff181ece48c3a21e2b79cb3b7bcd0d135652
skyline <telvintran@gmail.com>
2025-08-25 18:44:10 +0300

    final commit

--- app.py
+++ app.py
@@ -38,5 +38,28 @@ def index():
         tasks = Todo.query.order_by(Todo.date_created).all() #fetches all tasks from database
         return render_template("index.html", tasks=tasks)  # pass tasks to template
 
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task'
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('update.html', task=task)
+
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
