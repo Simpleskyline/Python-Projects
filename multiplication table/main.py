d3da5154941e55adac6e4b181927eccc18f76a1a
skyline <telvintran@gmail.com>
2025-08-25 10:24:00 +0300

    commit

--- multiplication table/main.py
+++ multiplication table/main.py
@@ -1,5 +1,8 @@
+#prompt the user to key in value
 num = int(input("Enter a number: "))
 
 print(f"\nMultiplication table of {num}\n")
+
+#for loop to repeat code 10 times
 for i in range(1, 11):
     print(f"{num} * {i} = {num * i}")
